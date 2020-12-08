0f43c4f83b5186fc1e31800041d361560769ddc2
Ginner Zapata <ginnerzapata@gmail.com>
2020-12-08 09:23:16 -0300

    Updates

---
+++ precache-manifest.b0e92f98eef2815777f9061632a7c8e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0602b79fb452491b7cd80360941a767c",
+    "url": "/react-markdown-previewer/index.html"
+  },
+  {
+    "revision": "854353ff6ab4542128f2",
+    "url": "/react-markdown-previewer/static/css/main.93e36b20.chunk.css"
+  },
+  {
+    "revision": "5a3c1dba299ed3a65f9b",
+    "url": "/react-markdown-previewer/static/js/2.84a4b586.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/react-markdown-previewer/static/js/2.84a4b586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854353ff6ab4542128f2",
+    "url": "/react-markdown-previewer/static/js/main.ce496085.chunk.js"
+  },
+  {
+    "revision": "2ab8beab2d110ceb541d",
+    "url": "/react-markdown-previewer/static/js/runtime-main.aaf84db4.js"
+  }
+]);
